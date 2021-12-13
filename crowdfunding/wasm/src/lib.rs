12eb4450dbbd4c5b2a948778616fd87b7721b0ff
Mircea Gherman <hi@mircea.me>
2021-12-13 19:47:52 +0200

    use crate version of the wasm dependency

---
+++ crowdfunding/wasm/src/lib.rs
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    crowdfunding
+    (
+        init
+        claim
+        fund
+        getCrowdfundingTokenIdentifier
+        getCurrentFunds
+        getDeadline
+        getDeposit
+        getTarget
+        status
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
